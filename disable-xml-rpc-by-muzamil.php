bd6470309b3ee534e46515f379166c36b9aa0af6
Muzamil Ahmad <129618208+Muzamilirf@users.noreply.github.com>
2024-03-24 21:43:12 +0500

    Add files via upload

---
+++ disable-xml-rpc-by-muzamil.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Block XML-RPC By Muzamil
+Description: Disables XML-RPC on your WordPress site for enhanced security.
+Version: 1.0
+Author: Muzamil Ahmad
+Author URI: https://www.linkedin.com/in/muzamilirf/
+*/
+
+// Main function to disable XML-RPC
+function block_xmlrpc_requests() {
+    // Let's do a check early on to see if this is even an XML-RPC request
+    if ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ) {
+        // This is an XML-RPC request, let's shut it down!
+        status_header( 403 ); // Send a Forbidden HTTP status code
+        die( 'XML-RPC requests are blocked on this site.' );
+    }
+}
+
+// Hook our function into the appropriate WordPress action
+add_action( 'init', 'block_xmlrpc_requests' ); 
